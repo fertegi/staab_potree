d7a0aa9c014812ecb64e2b948923120870304300
fertegi <ertegi@staab-architekten>
2025-11-26 17:03:05 +0100

    first commit

---
+++ src/exporter/XYZExporter.js
@@ -0,0 +1,30 @@
+export class XYZExporter {
+    static toString(points) {
+        let string = '';
+
+        let attributes = Object.keys(points.data)
+            .filter(a => a !== 'normal')
+            .sort((a, b) => {
+                if (a === 'position') return -1;
+                if (b === 'position') return 1;
+                if (a === 'rgba') return -1;
+                if (b === 'rgba') return 1;
+            });
+
+        for (let i = 0; i < points.numPoints; i++) {
+            let values = [];
+
+            for (let attribute of attributes) {
+                let itemSize = points.data[attribute].length / points.numPoints;
+                let value = points.data[attribute]
+                    .subarray(itemSize * i, itemSize * i + itemSize)
+                    .join('	');
+                values.push(value);
+            }
+
+            string += values.join(' ') + '\n';
+        }
+
+        return string;
+    }
+}
